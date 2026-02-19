5f189ef791b231321dab7b7767b67d1556963db4
EliasSteileh <elias.steileh@hotmail.com>
2026-02-19 06:58:27 +0100

    project init

---
+++ scripts/init_database.py
@@ -0,0 +1,19 @@
+
+import os
+import duckdb
+
+db_path = "DataWarehouse.duckdb"
+
+# Delete database file if exists
+if os.path.exists(db_path):
+    os.remove(db_path)
+
+# Create new database and schemas
+con = duckdb.connect(db_path)
+
+con.execute("CREATE SCHEMA bronze;")
+con.execute("CREATE SCHEMA silver;")
+con.execute("CREATE SCHEMA gold;")
+
+con.close()
+print("Database initialized successfully.")
